a0778da399eef638182dedd0b059fc21103c1da9
MatiasOubiña <matiasoubi@gmail.com>
2021-04-25 18:00:47 -0300

    V4 Proyecto Final JCK FOOD - Modificaciones Rev.6

--- php/formularios.php
+++ php/formularios.php
@@ -7,6 +7,6 @@ $correo = $_POST['email'];
 $asunto = $_POST['asunto'];
 $mensaje = $_POST['mensaje'];
 
-echo("<script> alert('Gracias $nombreCorreo por contactarte! Nos contactaremos al $correo en caso de ser necesario.');window.location='index.html' </script>");
+echo("<script> alert('Gracias $nombreCorreo por contactarte! Nos contactaremos al $correo en caso de ser necesario.');window.location='/index.html' </script>");
 
 ?>
