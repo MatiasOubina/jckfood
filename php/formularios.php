0e464a6aea818398048843fae6c750e8530e3291
MatiasOubiña <matiasoubi@gmail.com>
2021-04-25 17:17:51 -0300

    V4 Proyecto Final JCK FOOD - Modificaciones Rev.5

--- php/formularios.php
+++ php/formularios.php
@@ -1,16 +1,5 @@
 <?php
 
-//Formulario Reserva
-
-$nombreReserva = $_POST['nombre_apellido'];
-$telefono = $_POST['telefono'];
-$fechaReserva = $_POST['fecha_reserva'];
-$horaReseva = $_POST['hora_reserva'];
-$comensales = $_POST['total_comensales'];
-
-echo("<script> alert('Gracias $nombreReserva Tu reserva fue tomada! Nos contactaremos al $telefono por cualquier modificación o cancelación.')</script>");
-
-
 //Formulario Contacto
 
 $nombreContacto = $_POST['nombre'];
@@ -18,6 +7,6 @@ $correo = $_POST['email'];
 $asunto = $_POST['asunto'];
 $mensaje = $_POST['mensaje'];
 
-echo("<script> alert('Gracias $nombreCorreo por contactarte! Nos contactaremos al $correo en caso de ser necesario.')</script>");
+echo("<script> alert('Gracias $nombreCorreo por contactarte! Nos contactaremos al $correo en caso de ser necesario.');window.location='index.html' </script>");
 
 ?>
---
+++ php/reservas.php
@@ -0,0 +1,13 @@
+<?php
+
+//Formulario Reserva
+
+$nombreReserva = $_POST['nombre_apellido'];
+$telefono = $_POST['telefono'];
+$fechaReserva = $_POST['fecha_reserva'];
+$horaReseva = $_POST['hora_reserva'];
+$comensales = $_POST['total_comensales'];
+
+echo("<script> alert('Gracias $nombreReserva Tu reserva fue tomada! Nos contactaremos al $telefono por cualquier modificación o cancelación.');window.location='index.html' </script>");
+
+?>
